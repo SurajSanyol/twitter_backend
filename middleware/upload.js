56a01bb31e898ae1af5acd0fa0738ee2d8092b4d
SurajSanyol <surajkingsurajking438@gmail.com>
2024-09-10 14:28:28 +0530

    create uploadFiles folder into the controllers folder on the server

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,9 +1,17 @@
 import multer from 'multer'
+import fs from 'fs'
+import path from 'path';
 
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './controllers/uploadFiles')
+    const uploadDir = path.join(__dirname, 'controllers', 'uploadFiles');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+      console.log('uploadFiles directory created.');
+
+    }
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-';
